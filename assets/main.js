a3deed1e9c8ae03b537c969a0fc54bc6207e87b0
Débora Paulin <debipaulin@gmail.com>
2023-02-20 10:19:45 -0300

    editando arquivo .js

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,10 @@
+const numero = document.querySelector(".num");
 
+function mudaCor() {
+    numero.style.color = '#fb7413';
+}
 
-
-
-
+numero.addEventListener("click", mudaCor); 
+    function mudaCor() {
+        numero.style.color = 'green';
+    }
